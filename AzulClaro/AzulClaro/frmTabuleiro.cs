dcf7e48522c13ab958b881f8e47065ee32f7d12c
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2020-03-25 23:12:33 -0300

    Mostra azulejo em quadrado

--- AzulClaro/AzulClaro/frmTabuleiro.cs
+++ AzulClaro/AzulClaro/frmTabuleiro.cs
@@ -68,10 +68,20 @@ namespace AzulClaro
                 {
                     for (int j = 0; j < azul.quantidade; j++)
                     {
+                        if(i % 2 == 0)
+                        {
+                            ang = 90;
+                        }
+                        else
+                        {
+                            ang = 270;
+                        }
                         PictureBox pcbAzul = new PictureBox();
                         pcbAzul.Image = azul.image;
-                        pcbAzul.Location = new Point(fab.x + 50 * Convert.ToInt32(Math.Round(Math.Cos((ang * Math.PI)/180))), fab.y + 50 * Convert.ToInt32(Math.Round(Math.Sin(ang * Math.PI / 180))) - 50);
-                        ang += 90;
+                        int x = fab.x - 60;
+                        if (i > 1) x += 52;
+                        pcbAzul.Location = new Point(x, fab.y + 26 * Convert.ToInt32(Math.Round(Math.Sin(ang * Math.PI / 180))) - 55);
+                        i++;
                         pcbAzul.Width = 50;
                         pcbAzul.Height = 50;
                         pcbAzul.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -84,6 +94,10 @@ namespace AzulClaro
         //O porquê do cos(270) retornar -1
         //https://stackoverflow.com/questions/9652695/why-does-math-cos90-math-pi-180-yield-6-123031769111-and-not-zero
         //https://stackoverflow.com/questions/6082632/math-cos-math-sin-in-c-sharp
+        /* Define posição antigo, deixava os azulejos em cruz
+                   pcbAzul.Location = new Point(fab.x + 50 * Convert.ToInt32(Math.Round(Math.Cos((ang * Math.PI)/180))), fab.y + 50 * Convert.ToInt32(Math.Round(Math.Sin(ang * Math.PI / 180))) - 50);
+                   ang += 90;
+        */
         private void pcbFabricas_Click(object sender, EventArgs e)
         {
             MessageBox.Show(Cursor.Position.X + " , " + Cursor.Position.Y);
@@ -102,7 +116,7 @@ namespace AzulClaro
         //Define X e Y das fabricas na lista manualmente, pensar num jeito automatizado pra fazer
         public void definePos(int qtd)
         {
-            int[] posX = { 345, 577, 505, 183, 133 };
+            int[] posX = { 345, 577, 505, 183, 113 };
             int[] posY = { 185, 397, 667, 667, 397 };
             switch(qtd)
             {
