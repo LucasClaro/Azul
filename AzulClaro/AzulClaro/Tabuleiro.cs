3dd9e12f4f81699e677abbd41a283b23b8d5bece
Lucas Claro <lucasbrclaro@gmail.com>
2020-03-31 13:48:30 -0300

    Mais funções

---
+++ AzulClaro/AzulClaro/Tabuleiro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzulServer;
+
+namespace AzulClaro
+{
+    public class Tabuleiro
+    {
+        public bool[,,] parede { get; set; }
+        public Azulejo Fileira1 { get; set; }
+        public List<Azulejo> Fileira2 { get; set; }
+        public List<Azulejo> Fileira3 { get; set; }
+        public List<Azulejo> Fileira4 { get; set; }
+        public List<Azulejo> Fileira5 { get; set; }
+        public List<Azulejo> Chao { get; set; }        
+    }
+}
