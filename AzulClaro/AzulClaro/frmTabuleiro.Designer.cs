5a4067ea57d57097cfcfdc91025c8d3c28e06728
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2020-03-31 23:47:19 -0300

    Função atualiza azulejos e comentários

--- AzulClaro/AzulClaro/frmTabuleiro.Designer.cs
+++ AzulClaro/AzulClaro/frmTabuleiro.Designer.cs
@@ -31,6 +31,7 @@
             this.lblCabecalho = new System.Windows.Forms.Label();
             this.pcbFabricas = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pcbFabricas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,11 +63,22 @@
             this.textBox1.Size = new System.Drawing.Size(180, 341);
             this.textBox1.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(926, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
             // frmTabuleiro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pcbFabricas);
             this.Controls.Add(this.lblCabecalho);
@@ -87,5 +99,6 @@
         private System.Windows.Forms.Label lblCabecalho;
         private System.Windows.Forms.PictureBox pcbFabricas;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
